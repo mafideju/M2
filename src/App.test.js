943e05d22d0dc28bc7093457fc4515ab9dcdf5fa
Marcio Rodrigues <marcio.silva@CLUBRNB1098>
2019-08-23 10:41:52 -0300

    refactor(modulo-2): jest 02 => code coverage
    Co-authored-by: Marcio Mafideju <mafideju@outlook.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+const expectChai = require('chai').expect;
+const sum = require('./Sum');
 
-it('renders without crashing', () => {
-  ReactDOM.render(<App />, document.createElement('div'));
-  ReactDOM.unmountComponentAtNode(document.createElement('div'));
+
+describe('GRUPO PADRÃO DE TESTES', () => {
+  it('RENDERS WITHOUT CRASHING', () => {
+    ReactDOM.render(<App />, document.createElement('div'));
+    ReactDOM.unmountComponentAtNode(document.createElement('div'));
+  });
+
+  it('CONSOLE ASSERTION', () => {
+    // eslint-disable-next-line no-self-compare
+    console.assert(1 === 1, 'Numeros Iguais');
+  });
+
+  it('EXPECT DO JEST', () => {
+    expect(2).toBe(2);
+  });
+
+  it('SUM SHOULD BE A FUNCTION', () => {
+    expectChai(sum).to.be.a('function')
+  });
 });
---
+++ src/Sum.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// export function Sum(x, y) {
+//     return x + y;
+// }
+
+// Sum(1, 1);
+
+module.exports = () => {
+    if (true) {
+        return
+    }
+}
